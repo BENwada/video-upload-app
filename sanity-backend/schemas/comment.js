ca090c6569bdb5fb3cb4a35a2e729ef1f23fab18
BENwada <benwada@BENwadanoiMac.local>
2022-07-04 00:08:00 +0900

    first commit

---
+++ sanity-backend/schemas/comment.js
@@ -0,0 +1,17 @@
+export default {
+  name: "comment",
+  title: "Comment",
+  type: "document",
+  fields: [
+    {
+      name: "userName",
+      title: "User Name",
+      type: "string",
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "string",
+    },
+  ],
+};
