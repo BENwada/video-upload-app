8ef74e965e0a64946a247c2a34a8f8f1ccd06e8b
BENwada <domo0103wada@gmail.com>
2022-07-15 18:41:41 +0900

    topics added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import type { NextPage } from "next";
 import axios from "axios";
 import { Video } from "../types";
 import NoResults from "../components/NoResults";
@@ -9,9 +8,7 @@ interface IProps {
   videos: Video[];
 }
 
-const Home: NextPage = ({ videos }: IProps) => {
-  console.log(videos);
-
+const Home = ({ videos }: IProps) => {
   return (
     <div className="flex flex-col gap-10 videos h-full">
       {videos.length ? (
@@ -23,12 +20,21 @@ const Home: NextPage = ({ videos }: IProps) => {
   );
 };
 
-export const getServerSideProps = async () => {
-  const { data } = await axios.get(`${BASE_URL}/api/post`);
+export const getServerSideProps = async ({
+  query: { topic },
+}: {
+  query: { topic: string };
+}) => {
+  let response = null;
+  if (topic) {
+    response = await axios.get(`${BASE_URL}/api/discover/${topic}`);
+  } else {
+    response = await axios.get(`${BASE_URL}/api/post`);
+  }
 
   return {
     props: {
-      videos: data,
+      videos: response.data,
     },
   };
 };
